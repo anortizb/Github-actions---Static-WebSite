c65808bca4477bc1f34370e3d8e5e62ce73ac841
Andres Ortiz B <145217966+anortizb@users.noreply.github.com>
2026-04-17 11:47:20 -0500

    deploy

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,24 @@
+
 provider "aws" {
   region = "us-east-1"
 }
 
 terraform {
-  backend "s3" {
-    bucket  = "bucket-johansuesucnstate-terraform" # Debes crearlo manualmente una vez en AWS
-    key     = "github-actions-demo-andresortizb/terraform.tfstate" # Ruta dentro del bucket
-    region  = "us-east-1"
-    encrypt = true
+
+    backend "s3" {
+    bucket         = "bucket-johansuesucnstate-terraform" # Debes crearlo manualmente una vez
+    key            = "github-actions-demo-andresortizb/terraform.tfstate" # Ruta dentro del bucket
+    region         = "us-east-1"
+    encrypt        = true
   }
+  
 }
 
+
 resource "aws_s3_bucket" "web" {
-  bucket        = "andresortizb-githubactions-demo" # Lo crea Terraform automáticamente
-  force_destroy = true                               # Permite a Terraform vaciar el bucket al destruirlo
+  bucket = "andresortizb-githubaccions-demo"
 }
+
 
 resource "aws_s3_bucket_website_configuration" "web_config" {
   bucket = aws_s3_bucket.web.id
@@ -27,25 +31,25 @@ resource "aws_s3_bucket_website_configuration" "web_config" {
 resource "aws_s3_bucket_public_access_block" "web_public" {
   bucket = aws_s3_bucket.web.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
   restrict_public_buckets = false
 }
 
-# resource "aws_s3_bucket_policy" "public_read" {
-#   bucket = aws_s3_bucket.web.id
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.web.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Effect    = "Allow",
-#       Principal = "*",
-#       Action    = "s3:GetObject",
-#       Resource  = "arn:aws:s3:::andresortizb-githubactions-demo/*"
-#     }]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = "*",
+      Action = "s3:GetObject",
+      Resource = "arn:aws:s3:::andresortizb-githubaccions-demo/*"
+    }]
+  })
+}
 
 output "bucket_name" {
   value = aws_s3_bucket.web.bucket
