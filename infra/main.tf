60137adeb038e4568368e7a69ba8adb54c110f68
Andres Ortiz B <145217966+anortizb@users.noreply.github.com>
2026-04-17 11:30:43 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "bucket-johansuesucnstate-tfstate" # Debes crearlo manualmente una vez en AWS
+    bucket  = "bucket-johansuesucnstate-terraform" # Debes crearlo manualmente una vez en AWS
     key     = "github-actions-demo-andresortizb/terraform.tfstate" # Ruta dentro del bucket
     region  = "us-east-1"
     encrypt = true
