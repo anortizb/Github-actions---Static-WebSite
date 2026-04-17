3149b80abe9fe24b58c1703cfd304773f3b7c324
Andres Ortiz B <145217966+anortizb@users.noreply.github.com>
2026-04-17 11:28:40 -0500

    Update deploy.yml

--- infra/main.tf
+++ infra/main.tf
@@ -33,19 +33,19 @@ resource "aws_s3_bucket_public_access_block" "web_public" {
   restrict_public_buckets = false
 }
 
-# resource "aws_s3_bucket_policy" "public_read" {
-#   bucket = aws_s3_bucket.web.id
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.web.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Effect    = "Allow",
-#       Principal = "*",
-#       Action    = "s3:GetObject",
-#       Resource  = "arn:aws:s3:::andresortizb-githubactions-demo/*"
-#     }]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "arn:aws:s3:::andresortizb-githubactions-demo/*"
+    }]
+  })
+}
 
 output "bucket_name" {
   value = aws_s3_bucket.web.bucket
