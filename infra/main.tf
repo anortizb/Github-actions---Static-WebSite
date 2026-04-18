bb6f5df579b7c3d1d467cfb72b1295b1bc96f19b
Andres Ortiz B <145217966+anortizb@users.noreply.github.com>
2026-04-17 21:23:21 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -37,19 +37,19 @@ resource "aws_s3_bucket_public_access_block" "web_public" {
   restrict_public_buckets = false
 }
 
-# resource "aws_s3_bucket_policy" "public_read" {
-#   bucket = aws_s3_bucket.web.id
+ resource "aws_s3_bucket_policy" "public_read" {
+   bucket = aws_s3_bucket.web.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Effect = "Allow",
-#       Principal = "*",
-#       Action = "s3:GetObject",
-#       Resource = "arn:aws:s3:::andresortizb-githubaccions-demo/*"
-#     }]
-#   })
-# }
+   policy = jsonencode({
+     Version = "2012-10-17",
+     Statement = [{
+       Effect = "Allow",
+       Principal = "*",
+       Action = "s3:GetObject",
+       Resource = "arn:aws:s3:::andresortizb-githubaccions-demo/*"
+     }]
+   })
+}
 
 
 output "bucket_name" {
